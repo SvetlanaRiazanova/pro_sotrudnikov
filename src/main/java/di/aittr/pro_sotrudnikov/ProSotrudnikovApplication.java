c7d9885b7780b80736734c2acd74ea9987dc0c3d
SvetlanaRiazanova <89042480071s@gmail.com>
2025-04-08 16:29:03 +0400

    Initial commit

---
+++ src/main/java/di/aittr/pro_sotrudnikov/ProSotrudnikovApplication.java
@@ -0,0 +1,13 @@
+package di.aittr.pro_sotrudnikov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSotrudnikovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSotrudnikovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/di/aittr/pro_sotrudnikov/ProSotrudnikovApplicationTests.java
@@ -0,0 +1,13 @@
+package di.aittr.pro_sotrudnikov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSotrudnikovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
